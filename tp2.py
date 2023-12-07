88ae535d5f7358199189d3f95218079357895402
dipika08 <dipikapatel08@icloud.com>
2023-12-06 23:33:57 -0500

    bouton

--- tp2.py
+++ tp2.py
@@ -1,9 +1,7 @@
-# Vous devez remplacer le contenu de ce fichier par votre propre code
-# tel qu'indiqué dans la description du TP2.  Le code ici correspond
-# à l'exemple donné dans la description.
+# Auteurs: Dipika Patel, Junya Wang
+# Date: 9 décembre 2023
 
 import math
-# from xml.dom.minidom import Document
 
 elem = document.querySelector('#cb-body')
 
@@ -46,15 +44,17 @@ css = """
 elem.innerHTML = css
 deck = list(range(0,52))
 
-CARTES = [
- '10C.svg', '10D.svg', '10H.svg', '10S.svg', '2C.svg', '2D.svg', '2H.svg', 
- '2S.svg', '3C.svg', '3D.svg', '3H.svg', '3S.svg', '4C.svg', '4D.svg', 
- '4H.svg', '4S.svg', '5C.svg', '5D.svg', '5H.svg', '5S.svg', '6C.svg', 
- '6D.svg', '6H.svg', '6S.svg', '7C.svg', '7D.svg', '7H.svg', '7S.svg', 
- '8C.svg', '8D.svg', '8H.svg', '8S.svg', '9C.svg', '9D.svg', '9H.svg', 
- '9S.svg', 'AC.svg', 'AD.svg', 'AH.svg', 'AS.svg', 'JC.svg', 'JD.svg', 
- 'JH.svg', 'JS.svg', 'KC.svg', 'KD.svg', 'KH.svg', 'KS.svg', 'QC.svg', 
- 'QD.svg', 'QH.svg', 'QS.svg']
+chiffre = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+cartesC=[]
+cartesD=[]
+cartesH=[]
+cartesS=[]
+for i in range (len(chiffre)):
+    cartesC.append(chiffre[i]+'C.svg')
+    cartesD.append(chiffre[i]+'D.svg')
+    cartesH.append(chiffre[i]+'H.svg')
+    cartesS.append(chiffre[i]+'S.svg')
+CARTES = cartesC + cartesD + cartesH + cartesS
 
 def shuffle(deck):
     for i in range(len(deck) -1, -1, -1):
@@ -66,12 +66,13 @@ def shuffle(deck):
 
 def table(contenu): return '<table>' + contenu + '</table>'
 def tr(contenu): return '<tr>' + contenu + '</tr>'
-# def tr(contenu): return '<td>' + contenu + '</td>'
+def case (id_pos): return 'case' + str(id_pos)
 def td(contenu, id_pos): 
-    return '<td' + ' id=' + 'case' + str(id_pos) + '>' + contenu + '</td>'
+    return '<td' + ' id=' + case (id_pos) + '>' + contenu + '</td>'
 def img(card): return '<img src="cards/'+ card + '">'
 
 empty = []
+casel=[]
 
 def init():
     ROW = 4
@@ -84,58 +85,34 @@ def init():
         column = []
         for _ in range(COL):
             val = CARTES[cartes[c]]
+            
             if(not val.startswith('A')):
                 column.append(td(img(val),c))
             else:
-                column.append(td('',c))
-               # breakpoint()
-                empty.append(c-1)
+                column.append(td(img('absent.svg'),c))
+                
+                if c % 13 != 0 and ((CARTES[cartes[c-1]+1]).startswith('2') is False):
+                    casel.append(cartes.index(CARTES.index(CARTES[cartes[c-1]+1])))
+
             c += 1
         line.append(tr(''.join(column)))
    
     tbl = table(''.join(line))
     return tbl
 
-elem.innerHTML += init()
+msg = "Vous pouvez encore "
+html_brassage = '''
+<button id="but" onclick="clic()"> Brasser les cartes </button>
+<br><span id="msg"></span>
+<br>'''
+html_redemarre = '''
+<button id="but" onclick="clic()"> Nouvelle partie </button>
+<br><span id="msg"></span>
+<br>'''
+elem.innerHTML = css + init() + msg + html_brassage + html_redemarre
 
-def card_value(card):
-    card = card.split('.')[0]
-    ranks = '2345678910JQKA'
-    suits = 'CDHS'
-
-    rank = card[:-1]
-    suit = card[-1]
-
-    return ranks.index(rank), suits.index(suit)
-
-def find_higher_card(card, card_list):
-    current_rank, current_suit = card_value(card)
-
-    # Find a card with a higher rank and the same suit
-    for other_card in card_list:
-        other_rank, other_suit = card_value(other_card)
-
-        inc = 2 if current_rank == 8 else 1
-
-        # if other_suit == current_suit and other_rank == current_rank + inc:
-        #     return other_card
-
-        if other_suit == current_suit and other_rank+inc == current_rank + inc:
-            value = card_value(other_card)
-            print(value)
-
-    # If no higher card with the same suit is found
-    return None
-voisin = []
-for x in empty:
-    voisin.append(CARTES[deck[x]])   #finds the card left to empty
-for v in voisin :
-    find_higher_card(v,voisins)
-    # for x in deck: 
-    #     print(CARTES[deck[x]])
-              #case = document.querySelector('#'+ str(id_pos))
-    #case.setAttribute("style", "background-color: lime")
-
-#for i in voisin:
+for i in range (len(casel)):
+    case0 = document.querySelector("#"+case(casel[i]))
+    case0.setAttribute("style", "background-color: lime")
     
     
