404902f6219616776b62d385adf30345f1e2a03a
JunyaWang22 <59278443+JunyaWang22@users.noreply.github.com>
2023-12-10 18:14:18 -0500

    Update modi nom trouver_lime.py

--- modi nom trouver_lime.py
+++ modi nom trouver_lime.py
@@ -1,4 +1,53 @@
+# Auteurs: Dipika Patel, Junya Wang
+# Date: 9 décembre 2023
 
+import math
+
+racine = document.querySelector('#cb-body')
+
+css = """
+<style>
+#cb-body td {
+    width: 14vh;
+    height: 20vh;
+}
+
+#cb-body table td img {
+    width: 100%;
+    height: 100%;
+}
+</style>
+"""
+racine.innerHTML = css
+paquet = list(range(0,52))
+
+chiffre = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+cartesC=[]
+cartesD=[]
+cartesH=[]
+cartesS=[]
+for i in range (len(chiffre)):
+    cartesC.append(chiffre[i]+'C.svg')
+    cartesD.append(chiffre[i]+'D.svg')
+    cartesH.append(chiffre[i]+'H.svg')
+    cartesS.append(chiffre[i]+'S.svg')
+CARTES = cartesC + cartesD + cartesH + cartesS
+
+def brasser(paquet):
+    for i in range(len(paquet) -1, -1, -1):
+        j = math.floor(random() * (i+1))
+        tmp = paquet[i]
+        paquet[i] = paquet[j]
+        paquet[j] = tmp
+    return paquet
+
+def table(contenu): return '<table>' + contenu + '</table>'
+def tr(contenu): return '<tr>' + contenu + '</tr>'
+def case (id_pos): return '#case' + str(id_pos)
+def td(contenu, id_pos): 
+    return '<td' + ' id="' + case(id_pos) + '" onclick="clic(' + str(id_pos) + ')"' + '>' + contenu + '</td>'
+def img(card): return '<img src="cards/'+ card + '"/>'
+    
 trou = []
 index_lime=[]   
 cartes = brasser(paquet)
@@ -49,3 +98,64 @@ def trouver_lime(case):
 
     if case % COL != 0 and position not in A_ou_2:
         index_lime.append(cartes.index(position))
+
+def clic(id_pos):   
+    
+    carte = document.querySelector(case).innerHTML
+    a_change = carte[carte.find('/') + 1:carte.find('">')]
+    # print(a_change)
+    p = CARTES.index(a_change) - 1
+    index = cartes.index(p)
+    
+    # Move the card to the trou cell and trou its prior position
+    
+    if id_pos in index_lime:
+        document.querySelector('#case' + str(index+1)).innerHTML = '<img src="cards/' + name + '">'
+        document.querySelector(case).innerHTML = '<img src="cards/absent.svg">'
+        document.querySelector(case).removeAttribute("style")
+        trouver_idx(id_pos)
+        index_lime.remove(id_pos)
+        lime()
+
+def aligne():
+    for i in range (COL):
+        if column[:i]== cartesC[:i] or cartesD[:i] or cartesS[:i] or cartesH[:i]:
+            return column[i+1:]
+      
+# def cartes_index(carte):
+#    return cartes.index(CARTES.index(carte))
+#def clic(case):
+   # lime_card.replace('absent.svg')
+
+def brassage():
+    essai = 1
+    while essai <= 3:
+        aligne()
+        for _ in range (ROW):
+            cartei=brasser([i+1:])
+            for j in cartei:
+                column[:i].append(CARTES[j])
+    essai += 1
+    
+    msg = "Vous pouvez encore "
+    html_brassage = '''<button id="brasser" onclick="brassage()">Brasser les cartes </button>'''
+    msg1 = str(essai) + "fois\n"
+    return msg + html_brassage + msg1
+    
+
+
+html_redemarre = "<button id=""redemarre"" onclick=redemarre()> Nouvelle partie </button>"
+
+def redemarre():
+    brasser(paquet)
+    racine.innerHTML = css + init() + brassage() + html_redemarre
+    lime()
+    
+def result():
+    if line == CARTES:
+        print("Vous avez réussi!  Bravo!")
+    if idx_cartes_lime == [] and brassage()==False and line != CARTES:
+        print("Vous n'avez pas réussi à placer toutes les cartes... Essayez à nouveau!")
+        
+racine.innerHTML = css + init() + brassage() + html_redemarre
+lime()
