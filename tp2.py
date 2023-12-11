21bdea1b328d905a8b9d6680fd19bdf4b815fc54
JunyaWang22 <59278443+JunyaWang22@users.noreply.github.com>
2023-12-11 09:51:34 -0500

    Update tp2.py

--- tp2.py
+++ tp2.py
@@ -3,46 +3,23 @@
 
 import math
 
-elem = document.querySelector('#cb-body')
+racine = document.querySelector('#cb-body')
 
 css = """
 <style>
-#cb-body { font-size: 24px; }
-#cb-body td { border: 1px solid black; }
-#cb-body table {
-    float: center;
-    table-layout: fixed;
-    border-collapse: collapse;
-}
 #cb-body td {
-    padding: 0;
-    font-family: Helvetica;
-    text-align: center;
-    vertical-align: center;
-    border: 1px solid black;
-}
-
-#cb-body td {
-    width: 100px;
-    height: 100px;
-    font-size: 60px;
+    width: 14vh;
+    height: 20vh;
 }
 
 #cb-body table td img {
-    display: block;
-    object-fit: contain;
-    vertical-align: middle;
     width: 100%;
     height: 100%;
 }
-
-#cb-body { float: none; }
-#cb-body table td { border: 0; padding: 1px 2px; height: auto; }
-#cb-body table td img { height: auto; }
 </style>
 """
-elem.innerHTML = css
-deck = list(range(0,52))
+racine.innerHTML = css
+paquet = list(range(0,52))
 
 chiffre = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 cartesC=[]
@@ -56,111 +33,130 @@ for i in range (len(chiffre)):
     cartesS.append(chiffre[i]+'S.svg')
 CARTES = cartesC + cartesD + cartesH + cartesS
 
-def shuffle(deck):
-    for i in range(len(deck) -1, -1, -1):
+def brasser(paquet):
+    for i in range(len(paquet) -1, -1, -1):
         j = math.floor(random() * (i+1))
-        tmp = deck[i]
-        deck[i] = deck[j]
-        deck[j] = tmp
-    return deck
+        tmp = paquet[i]
+        paquet[i] = paquet[j]
+        paquet[j] = tmp
+    return paquet
 
 def table(contenu): return '<table>' + contenu + '</table>'
 def tr(contenu): return '<tr>' + contenu + '</tr>'
-def case (id_pos): return 'case' + str(id_pos)
+def case (id_pos): return '#case' + str(id_pos)
 def td(contenu, id_pos): 
-    return '<td' + ' id=' + case (id_pos) + '>' + contenu + '</td>'
-def img(card): return '<img src="cards/'+ card + '">'
-def tdid(id_pos, contenu):
-    return '<td' + ' id=' + case (id_pos) + "onclick=" + contenu + '>' + '</td>'
+    return '<td' + ' id="' + 'case' + str(id_pos) + '" onclick="clic(' + str(id_pos) + ')"' + '>' + contenu + '</td>'
+def img(card): return '<img src="cards/'+ card + '"/>'
 
-empty = []
-COL=13
-ROW=4
 
-def val(cartes,indice,position):
-    return CARTES[cartes[indice]+position]  
+trou = []
+index_lime=[]   
+cartes = brasser(paquet)
+ROW = 4
+COL = 13
+
+#def val(cartes,indice,position):
+#    return CARTES[cartes[indice]+position]  
+
     
 def init():
-    global casel, line
-    casel=[]
-    c = 0
+    global cartes
+    i = 0 
+
     line = []
-    cartes = shuffle(deck)
+    
     for _ in range(ROW):
-        global column
         column = []
         for _ in range(COL):
-            val = CARTES[cartes[c]]
+            value = CARTES[cartes[i]]
             
-            if(not val.startswith('A')):
-                column.append(td(img(val),c))
+            if paquet[i] % COL != 0:
+                column.append(td(img(value),i))
             else:
-                column.append(td(img('absent.svg'),c))
-                lime_card = CARTES[cartes[c-1]+1]
-                if c % COL == 0:
-                    i = 1
-                    while i < COL*ROW:
-                        casel.append(cartes.index(i))
-                        i += COL
-
-                if c % COL != 0 and (not (lime_card).startswith('A')) and  (not (lime_card).startswith('2')):    
-                    casel.append(cartes.index(CARTES.index(lime_card)))
-                    print(casel)
-                    sleep(0.3)
-                    print(column)
-                    print(c)
-                    column.pop(c%COL)
-                    column.insert(c%COL,td(img(lime_card),c%COL))
-                    #line.append(tdid((cartes.index(CARTES.index(lime_card))),"lime_card.replace('absent.svg')"))
-                    # onclick = clic(cartes.index(CARTES.index(lime_card)))
-            c += 1
+                column.append(td(img('absent.svg'),i))
+                trou.append(cartes.index(i))
+                trouver_lime(i)
+            i += 1
         line.append(tr(''.join(column)))
-    
+   
+    tableau = table(''.join(line))
+    return tableau
 
-    tbl = table(''.join(line))
-    return tbl    
-        # onclick = clic(cartes.index(CARTES.index(lime_card)))
+def lime():
+    for i in range (len(index_lime)):
+        case0 = document.querySelector(case(index_lime[i]))
+        case0.setAttribute("style", "background-color: lime")
 
-#def clic():
+def trouver_lime(case):
+    A_ou_2 = [0,13,26,39,1,14,27,40]
+    position = cartes[case-1]+1
     
+    if case % COL == 0:
+        i = 1
+        while i < COL*ROW:
+            index_lime.append(cartes.index(i))
+            i += COL
+
+    if case % COL != 0 and (position not in A_ou_2):
+        index_lime.append(cartes.index(position))
+
+def clic(id_pos):   
+    
+    carte = document.querySelector(case(id_pos)).innerHTML
+    a_change = carte[carte.find('/') + 1:carte.find('">')]
+    # print(a_change)
+    p = CARTES.index(a_change) - 1
+    index = cartes.index(p)
+    
+    # Move the card to the trou cell and trou its prior position
+    
+    if id_pos in index_lime:
+        document.querySelector(case(index+1)).innerHTML = '<img src="cards/' + a_change + '">'
+        document.querySelector(case(id_pos)).innerHTML = '<img src="cards/absent.svg">'
+        document.querySelector(case(id_pos)).removeAttribute("style")
+        trouver_lime(id_pos)
+        index_lime.remove(id_pos)
+        lime()
 
 def aligne():
     for i in range (COL):
         if column[:i]== cartesC[:i] or cartesD[:i] or cartesS[:i] or cartesH[:i]:
-            return True
+            return column[i+1:]
       
 # def cartes_index(carte):
 #    return cartes.index(CARTES.index(carte))
 #def clic(case):
    # lime_card.replace('absent.svg')
 
-def brassage():
-    print("hello")
-    elem.innerHTML = css + init() + msg + html_brassage + html_redemarre
+def brassage(liste):
+    essai = 1
+    while essai <= 3:
+        aligne()
+        for _ in range (ROW):
+            cartei=brassage(column[(i+1):])
+            for j in cartei:
+                column[:i].append(CARTES[j])
+    essai += 1
+    
+    msg = "Vous pouvez encore "
+    html_brassage = '''<button id="brasser" onclick="brassage(column[(i+1):])">Brasser les cartes </button>'''
+    msg1 = str(essai) + "fois\n"
+    return msg + html_brassage + msg1
+    
 
+
+html_redemarre = "<button id=""redemarre"" onclick=redemarre()> Nouvelle partie </button>"
 
 def redemarre():
-    elem.innerHTML = css + init() + msg + html_brassage + html_redemarre
-
+    brasser(paquet)
+    racine.innerHTML = css + init() + brassage() + html_redemarre
+    lime()
+    
 def result():
     if line == CARTES:
         print("Vous avez réussi!  Bravo!")
-    if casel == [] and brassage()==False and line != CARTES:
+    if idx_cartes_lime == [] and brassage()==False and line != CARTES:
         print("Vous n'avez pas réussi à placer toutes les cartes... Essayez à nouveau!")
-
-    
-msg = "Vous pouvez encore "
-html_brassage = '''
-<button id="but" onclick="brassage()"> Brasser les cartes </button>
-<br><span id="msg"></span>
-<br>'''
-html_redemarre = '''
-<button id="but" onclick="redemarre()"+"removeAttribute"> Nouvelle partie </button>
-<br><span id="msg"></span>
-<br>'''
-elem.innerHTML = css + init() + msg + html_brassage + html_redemarre #+ result()
-
-for i in range(len(casel)):
-        case0 = document.querySelector("#"+case(casel[i]))
-        case0.setAttribute("style", "background-color: lime")
-    #<td>id="case0" onclick="removeAttribute"
+        
+racine.innerHTML = css + init() #+ brassage(column[(i+1):]) + html_redemarre
+lime()
